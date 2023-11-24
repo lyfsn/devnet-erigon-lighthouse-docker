16a5dff980f1c24242387005798447c01f71c81b
wangyufsn <dev.wangyu@proton.me>
2023-11-24 14:26:22 +0800

    up

---
+++ initExecutor.sh
@@ -0,0 +1,13 @@
+
+
+docker run \
+  --rm \
+  -it \
+  -v $(pwd)/execution-data:/execution-data \
+  -v $(pwd)/config-example:/config \
+  -v $(pwd)/el-cl-genesis-data:/el-cl-genesis-data \
+  thorax/erigon:stable \
+  init \
+  --datadir=/execution-data \
+  /el-cl-genesis-data/custom_config_data/genesis.json
+
